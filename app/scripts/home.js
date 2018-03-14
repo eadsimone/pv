68b97077855e464e0d9f3a8ccc4f8c9833da3853
eadsimone <eadesimone@gmail.com>
2018-03-14 09:37:45 -0300

    fixing issues

--- app/scripts/home.js
+++ app/scripts/home.js
@@ -11,10 +11,10 @@ class Home {
       '    "userId": 1,' +
       '    "id": 1,' +
       '    "visited": 0,' +
-      '    "title": "City tour por Balcarce",' +
+      '    "title": "Plaza libertad",' +
       '    "info": "Como en la mayoría de los pueblos bonaerenses, la ciudad de Balcarce se mueve al compás de su plaza principal, que en este caso ocupa nada más y nada menos que cuatro formidables manzanas. Los tilos aportan la sombra adecuada para descansar bajo sus ramas y las pérgolas cubiertas de enredaderas añaden estilo romántico a este paraje, ubicado a 407 kilómetros de la Capital Federal.",' +
       '    "moreinfo" : "En el centro de la Plaza Libertad se encuentra el monumento a la República, erigido sobre la Pirámide de Mayo. Diversas estatuas y canteros de flores invitan a un apacible recorrido a pie. La plaza es el eje de la vida balcarceña, donde se llevan a cabo las principales festividades de la ciudad y se instalan algunos puestos de artesanías, inspirados mayormente en la temática automovilística.     Alrededor de la Plaza Libertad se concentra el conjunto de los edificios destacados de la población, como el Palacio Municipal -un edificio del año 1929-, la fuente con el monumento al brigadier general Antonio González Balcarce, quien fue Segundo Jefe del Ejército de los Andes, el Banco de la Nación Argentina, el Museo del Automovilismo Juan Manuel Fangio y la Iglesia San José, de estilo romano, que data del año 1886.",'+
-      '    "image": "https://www.welcomeargentina.com/paseos/recorrido_balcarce/balcarce-city-tour-1.jpg"' +
+      '    "image": "/images/fotositems/Plaza Libertad.jpg"' +
       '  },' +
       '  {' +
       '    "userId": 1,' +
@@ -23,25 +23,25 @@ class Home {
       '    "title": "El auténtico postre Balcarce",' +
       '    "info": "est rerum tempore vitae\\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\\nqui aperiam non debitis possimus qui neque nisi nulla",' +
       '    "moreinfo": "Lorem ipsum dolor sit amet, nam legimus explicari eu, eam cu posse torquatos persecuti, nominati sadipscing eu vel. In tale prompta vituperatoribus pro. Mandamus consetetur dissentias ut sit, an est viris recteque. Ubique indoctum mel no, magna sanctus definitiones ne quo. Solum intellegam duo ne, sea oblique disputationi ne, duo an dolor ubique. Cu eius lucilius definiebas eum.",'+
-      '    "image": "https://www.welcomeargentina.com/paseos/postre_balcarce/postre-balcarce-1.jpg"' +
+      '    "image": "/images/fotositems/PostreBalcarce.jpg"' +
       '  },' +
       '  {' +
       '    "userId": 1,' +
       '    "id": 3,' +
       '    "visited": 0,' +
-      '    "title": "La Barrosa, aventura en la sierra",' +
+      '    "title": "Senderos en la sierra",' +
       '    "info": "et iusto sed quo iure\\nvoluptatem occaecati omnis eligendi aut ad\\nvoluptatem doloribus vel accusantium quis pariatur\\nmolestiae porro eius odio et labore et velit aut",' +
       '    "moreinfo": "Lorem ipsum dolor sit amet, nam legimus explicari eu, eam cu posse torquatos persecuti, nominati sadipscing eu vel. In tale prompta vituperatoribus pro. Mandamus consetetur dissentias ut sit, an est viris recteque. Ubique indoctum mel no, magna sanctus definitiones ne quo. Solum intellegam duo ne, sea oblique disputationi ne, duo an dolor ubique. Cu eius lucilius definiebas eum.",'+
-      '    "image": "https://www.welcomeargentina.com/paseos/la_barrosa/la-barrosa-1.jpg"' +
+      '    "image": "/images/fotositems/SenderosenlasSierras.jpg"' +
       '  },' +
       '  {' +
       '    "userId": 1,' +
       '    "id": 4,' +
       '    "visited": 0,' +
-      '    "title": "Los autos de fórmula también dicen presente",' +
+      '    "title": "Museo Historico",' +
       '    "info": "ullam et saepe reiciendis voluptatem adipisci\\nsit amet autem assumenda provident rerum culpa\\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\\nquis sunt voluptatem rerum illo velit",' +
       '    "moreinfo": "Lorem ipsum dolor sit amet, nam legimus explicari eu, eam cu posse torquatos persecuti, nominati sadipscing eu vel. In tale prompta vituperatoribus pro. Mandamus consetetur dissentias ut sit, an est viris recteque. Ubique indoctum mel no, magna sanctus definitiones ne quo. Solum intellegam duo ne, sea oblique disputationi ne, duo an dolor ubique. Cu eius lucilius definiebas eum.",'+
-      '    "image": "https://www.welcomeargentina.com/paseos/crespi_competicion/tulio-crespi-competicion-1.jpg"' +
+      '    "image": "/images/fotositems/museohistorico.jpg"' +
       '  },' +
       '  {' +
       '    "userId": 1,' +
@@ -59,7 +59,7 @@ class Home {
       '    "title": "Museo Fangio",' +
       '    "info": "ut aspernatur corporis harum nihil quis provident sequi\\nmollitia nobis aliquid molestiae\\nperspiciatis et ea nemo ab reprehenderit accusantium quas\\nvoluptate dolores velit et doloremque molestiae",' +
       '    "moreinfo": "Lorem ipsum dolor sit amet, nam legimus explicari eu, eam cu posse torquatos persecuti, nominati sadipscing eu vel. In tale prompta vituperatoribus pro. Mandamus consetetur dissentias ut sit, an est viris recteque. Ubique indoctum mel no, magna sanctus definitiones ne quo. Solum intellegam duo ne, sea oblique disputationi ne, duo an dolor ubique. Cu eius lucilius definiebas eum.",'+
-      '    "image": "https://www.welcomeargentina.com/paseos/museo_fangio/juan-manuel-fangio-1.jpg"' +
+      '    "image": "/images/fotositems/museofangio.jpg"' +
       '  }' +
       ']';
   }
@@ -132,11 +132,13 @@ class Home {
         '       <div id="collapseExample">' +
         '         <div class="">'+ obj.info +'</div>' +
         '       </div>' +
-        '       <a class="btn btn-primary-wwepc readmore readmoreinfo" role="button">Read More</a> ' +
+        // '       <a class="btn btn-primary-wwepc readmore readmoreinfo" role="button">Read More</a> ' +
+        '<button type="button" class="btn btn-primary-pv readmore readmoreinfo">Leer más</button>'+
         '     </div>' +
         '   </div>' +
         '   <div id="desafio" class="p-3">' +
-        '     <a class="btn btn-primary-wwepc readmore discount" role="button">Comparte tu foto y obten tu descuento!!!</a> ' +
+        // '     <a class="btn btn-primary-wwepc readmore discount" role="button">Comparte tu foto y obten tu descuento!!!</a> ' +
+        '     <button type="button" class="btn btn-primary-pv readmore discount">Comparte tu foto y obten tu descuento!!!</button>'+
         '   </div>' +
         '   <div id="location" class="google-maps">' +
              '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3150.592766012663!2d-58.257691784680304!3d-37.8464184797465!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x959aaf15b0657f65%3A0xbd1f381ba07edf7!2sPlaza+Libertad!5e0!3m2!1sen!2sar!4v1516214484703" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>' +
